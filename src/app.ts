197ac9fc60669ba1bd4d54223bc99bf72e6d29f0
Leandro Assis <leandrou9@gmail.com>
2022-12-28 11:32:55 -0300

    Definindo funcionalidades da aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('oioioi');
+const form = document.querySelector('form')!;
+const enderecoInput = document.getElementById('endereco')! as HTMLInputElement;
+
+function procuraEndereco(evento: Event) {
+    evento.preventDefault();
+    const enderecoEntrada = enderecoInput.value;
+
+    //Enviar endereco para API do Google
+}
+
+form.addEventListener('envio', procuraEndereco);
